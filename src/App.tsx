ae4b00cb3ce6058079fa9141af4966ca7df19cce
Jasleen Kaur <jkaurdhillon51@gmail.com>
2025-09-18 16:57:13 +0530

    Initial commit : Updated HeroSection empire-frontend

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col">
+    <div className="min-h-screen bg-[#D7E0E7] flex flex-col">
       <Navbar />
       <main className="flex-grow">
         <Home/>
