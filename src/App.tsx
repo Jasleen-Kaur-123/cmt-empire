fb090eccd46f6281f854e70508beb3a596483c59
Jasleen Kaur <jkaurdhillon51@gmail.com>
2025-09-18 05:50:42 +0530

    Fix case-sensitive import issue for Homesection

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import HomeSection from "./components/Homesection";
+import Homesection from "./components/Homesection";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col">
       <Navbar />
       <main className="flex-grow">
-        <HomeSection />
+        <Homesection />
       </main>
       <Footer />
     </div>
--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,4 +1,4 @@
-export default function HomeSection() {
+export default function Homesection() {
   const cards = [
     {
       name: "Anthoxanthin",
